1b86ded4471ad846cb89b06ce5ad4ef7798181f7
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2024-06-05 21:20:20 +0900

    240605

---
+++ Programmers/42884-fail/1.py
@@ -0,0 +1,12 @@
+def solution(routes):
+    answer = 0
+    
+    routes.sort(key = lambda x: x[1])
+    camera = -30001
+    
+    for r in routes:
+        if camera < r[0]:
+            answer += 1
+            camera = r[1]   
+    
+    return answer
