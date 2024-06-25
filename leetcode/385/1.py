a942ab6ba69bf0049d900d38a4499e63685267b6
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2024-06-25 23:03:13 +0900

    240625

---
+++ leetcode/385/1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def deserialize(self, s: str) -> NestedInteger:
+        return self.find(eval(s))
+    
+    def find(self, s) -> NestedInteger:
+        if type(s) == type(1):
+            return NestedInteger(s)
+        
+        n = NestedInteger()
+
+        for x in s:
+            n.add(self.find(x))
+        return n
+        
